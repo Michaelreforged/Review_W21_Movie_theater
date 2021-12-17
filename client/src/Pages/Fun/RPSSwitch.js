47c1eddc4471432474f09add46792b914d268185
Michael Cheung <michaelcheung.cheme@gmail.com>
2021-12-17 15:43:22 -0700

    RPS test files done

--- client/src/Pages/Fun/RPSSwitch.js
+++ client/src/Pages/Fun/RPSSwitch.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Imagestd } from "./Styles";
 import Squirtle from "./Images/Squirtle.webp"
 import Charmander from "./Images/Charmander.webp"
@@ -7,28 +7,78 @@ import Bulbasaur from "./Images/Bulbasaur.webp"
 const RPSWSwitch = () =>{
   const[player,setPlayer] = useState(null);
   const[comp,setComp] = useState(null);
-  const[score,setScore] = useState({player: 0, comp: 0, draw: 0});
-  const [record,setRecord] = useState([])
-  const [clear,setClear] = useState(false)
-
+  const [winner,setWinner] = useState(false)
   const options =  ["r", "p", "s"];
+
+  useEffect(()=>{
+    console.log(player, comp)
+    win()
+    
+  },[player])
+
+  const renderWinner = () =>{
+    return<h1>{winner}</h1>
+  }
+
+  const win = () => {
+    setComp(options[Math.floor(Math.random()*3)])
+    switch (true) {
+
+      case (comp !== null && comp === player):
+        setWinner("Tie")
+        break;
+
+      case (player === options[0]):
+        if(comp === options[1]){
+          setWinner("Computer")
+        } else{
+          setWinner("Player")
+        }
+        break;
+
+      case (player === options[1]):
+        if(comp === options[2]){
+          setWinner("Computer")
+        } else{
+          setWinner("Player")
+        }
+        break;
+
+      case (player === options[2]):
+        if(comp === options[0]){
+          setWinner("Computer")
+        } else{
+          setWinner("Player")
+        }
+        break;
+        
+      default:
+        if(comp === null){
+          console.log(comp)
+          return setWinner("Start Game")
+        }
+        break;
+    }
+    return setPlayer("")
+  }
+
+  const reset = () => {
+    setComp(null)
+    setPlayer(null)
+  }
 
   return (
     <div>
       <h1>Welcome to Rock Paper Scissors</h1>
+      { renderWinner()}
       <div>
-        <Imagestd src={Squirtle} />
-        <Imagestd src={Bulbasaur} />
-        <Imagestd src={Charmander} />
+        <Imagestd src={Squirtle} onClick = {()=>setPlayer(options[0])} />
+        <Imagestd src={Bulbasaur} onClick= {()=>setPlayer(options[1])} />
+        <Imagestd src={Charmander} onClick= {()=>setPlayer(options[2])} />
       </div>
-      <div>
-        {/* <p>Player Score: {score.player} Computer Score: {score.comp} Draws: {score.draw}</p> */}
-        </div>
-        {/* <button onClick= {()=> setClear(true)}>Clear</button> */}
-        {/* {renderRecord()} */}
+      <button onClick={()=>{reset()}}>Reset</button>
     </div>
   );
-
 }
 
 export default RPSWSwitch

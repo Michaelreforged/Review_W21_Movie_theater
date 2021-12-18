d7e393439d1be1ba19959f603825aa99f94ecd95
Michael Cheung <michaelcheung.cheme@gmail.com>
2021-12-17 17:14:30 -0700

    functioning rps

--- client/src/Pages/Fun/RPSReducer.js
+++ client/src/Pages/Fun/RPSReducer.js
@@ -1,25 +1,57 @@
-import React from "react";
+import React, { useReducer } from "react";
 import { Imagestd } from "./Styles";
 import Squirtle from "./Images/Squirtle.webp"
 import Charmander from "./Images/Charmander.webp"
 import Bulbasaur from "./Images/Bulbasaur.webp"
 
 const RPSReducer = () =>{
+  const options =  ["r", "p", "s"];
+  const intialState = {winner: "Start game"}
+  const [state, dispatch] = useReducer(win, intialState)
+
+
+  function win(state,choice) {
+    let comp = (options[Math.floor(Math.random()*3)])
+    console.log("c",comp)
+    console.log("P",choice.option)
+    switch (choice.option){
+      case "r":
+      if( comp === options[1]){
+        return{winner: "Computer Wins"}
+      } else if( comp === options[2]){
+        return{winner:"Player Wins"}
+      } else{ return {winner:"tie"}}
+      break
+      case "s":
+      if( comp === options[0]){
+        return{winner: "Computer Wins"}
+      } else if( comp === options[1]){
+        return{winner:"Player Wins"}
+      } else{ return {winner:"tie"}}
+      break
+      case "p":
+      if( comp === options[1]){
+        return{winner: "Computer Wins"}
+      } else if( comp === options[0]){
+        return{winner:"Player Wins"}
+      } else{ return {winner:"tie"}}
+      break
+      default:
+        return {winner:"Start Game"}
+    }
+  }
 
 
   return(
     <div>
       <h1>Welcome to Rock Paper Scissors</h1>
+      <h1>{state.winner}</h1>
       <div>
-        <Imagestd src={Squirtle} />
-        <Imagestd src={Bulbasaur} />
-        <Imagestd src={Charmander} />
+        <Imagestd src={Squirtle} onClick={()=>{dispatch({option: options[0]})}}/>
+        <Imagestd src={Bulbasaur} onClick={()=>{dispatch({option: options[1]})}}/>
+        <Imagestd src={Charmander} onClick={()=>{dispatch({option: options[2]})}}/>
       </div>
-      <div>
-        {/* <p>Player Score: {score.player} Computer Score: {score.comp} Draws: {score.draw}</p> */}
-        </div>
-        {/* <button onClick= {()=> setClear(true)}>Clear</button> */}
-        {/* {renderRecord()} */}
+        <button onClick={()=>dispatch({options: "reset"})}>RESET</button>
     </div>
   )
 }
--- client/src/Pages/Fun/RPSSwitch.js
+++ client/src/Pages/Fun/RPSSwitch.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Imagestd } from "./Styles";
 import Squirtle from "./Images/Squirtle.webp"
 import Charmander from "./Images/Charmander.webp"
@@ -7,25 +7,67 @@ import Bulbasaur from "./Images/Bulbasaur.webp"
 const RPSWSwitch = () =>{
   const[player,setPlayer] = useState(null);
   const[comp,setComp] = useState(null);
-  const[score,setScore] = useState({player: 0, comp: 0, draw: 0});
-  const [record,setRecord] = useState([])
-  const [clear,setClear] = useState(false)
+  const [winner, setWinner] = useState("Start Game")
 
   const options =  ["r", "p", "s"];
+
+  useEffect(()=>{
+    win()
+  },[player])
+
+  const win = () => {
+    if(player === ""){
+      return
+    }
+    setComp(options[Math.floor(Math.random()*3)])
+    console.log("p",player)
+    console.log("c",comp)
+    switch (true){
+      case (comp !== null && player === comp):
+        setWinner("Tie")
+      break
+      case (player === options[0]):
+      if (comp === options[1]){
+        setWinner("Computer Wins")
+      }
+      else(setWinner("Player Wins"))
+      break
+      case (player === options[1]):
+      if (comp === options[2]){
+        setWinner("Computer Wins")
+      }
+      else(setWinner("Player Wins"))
+      break
+      case (player === options[2]):
+      if (comp === options[0]){
+        setWinner("Computer Wins")
+      }
+      else(setWinner("Player Wins"))
+      break
+      default:
+        if (comp === null){
+          return setWinner("Start Game")
+        }
+        break
+    }
+    setPlayer("")
+  }
+
+  const resetGame = () => {
+    setPlayer(null)
+    setComp(null)
+  }
 
   return (
     <div>
       <h1>Welcome to Rock Paper Scissors</h1>
+      <h1> {winner}</h1>
       <div>
-        <Imagestd src={Squirtle} />
-        <Imagestd src={Bulbasaur} />
-        <Imagestd src={Charmander} />
+        <Imagestd src={Squirtle} onClick={() => setPlayer(options[0])}/>
+        <Imagestd src={Bulbasaur} onClick={() => setPlayer(options[1])}/>
+        <Imagestd src={Charmander} onClick={() => setPlayer(options[2])}/>
       </div>
-      <div>
-        {/* <p>Player Score: {score.player} Computer Score: {score.comp} Draws: {score.draw}</p> */}
-        </div>
-        {/* <button onClick= {()=> setClear(true)}>Clear</button> */}
-        {/* {renderRecord()} */}
+      <button onClick = {()=>resetGame()}>RESET</button>
     </div>
   );
 

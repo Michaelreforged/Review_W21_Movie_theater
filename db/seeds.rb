6ce11f489284d273ac330181eacf34c67c4928a4
Michael Cheung <michaelcheung.cheme@gmail.com>
2022-01-14 14:49:00 -0700

    update to seeds and updated yarn

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,8 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+require 'faker'
+
 Theater.destroy_all
 
 theater1 = Theater.create(name: "Cinemark", loc:"Main St.")
@@ -17,3 +19,15 @@ spiderman2 = theater2.movies.create(name:"Spider-man 2", duration:" 1 hour 30 mi
 spiderman3 = theater3.movies.create(name:"Spider-man 3", duration:" 1 hours 45 mins")
 
 spiderman4 = Movie.create(name:"The Amazing Spider-Man", duration:"3 hours", theater_id: Theater.first.id)
+
+@times = [" 1 hours"," 1 hours 10 mins"," 1 hours 20 mins"," 1 hours 30 mins"," 1 hours 40 mins"," 1 hours 50 mins"," 2 hours"," 2 hours 10 mins"," 2 hours 20 mins"," 2 hours 30 mins"]
+
+@theater = [theater1,theater2,theater3]
+
+100.times do 
+  Movie.create(name: Faker::Movie.title, duration: @times[rand(0..9)], theater_id: @theater[rand(0..2)].id)
+end
+
+p "========================HOW MANY SEEDS========================"
+p "# of theaters = #{Theater.count}"
+p "# of Movies = #{Movie.count}"
